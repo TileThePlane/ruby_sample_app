da9bcbcfbee016a611404f583364e6c430d9f9eb
Kyle Vonderwerth <kvonderw@uci.edu>
2018-10-23 20:31:57 -0700

    Add user email validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email:"@example.com")
+    @user = User.new(name: "Example User", email:"user@example.com")
   end
 
   test "should be valid" do
@@ -27,5 +27,14 @@ class UserTest < ActiveSupport::TestCase
   test "emails should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_address = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_address.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
   end
 end
