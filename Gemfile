59e86d601155144f03675b8a550ba85429e020e3
Kyle Vonderwerth <kvonderw@uci.edu>
2018-10-23 21:03:48 -0700

    Add user passwords

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.5.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
+gem 'bcrypt'
 gem 'bootstrap-sass', '3.2.0.2'
 gem 'pg'
 # Use Puma as the app server
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
-    @user = User.new(name: "Example User", email:"user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -29,7 +31,6 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-<<<<<<< HEAD
   test "email validation should accept valid addresses" do
     valid_address = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -37,12 +38,23 @@ class UserTest < ActiveSupport::TestCase
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
-=======
+  end
+
   test "email address should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
->>>>>>> modeling-users
   end
+
+    test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
