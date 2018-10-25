9aff8cd49ccb14178fcd6dba607b60ff2f25f518
Kyle Vonderwerth <kvonderw@uci.edu>
2018-10-25 15:17:42 -0700

    Test layout correctness on user login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-
+# 1
 #    Visit the login path.
 #    Verify that the new sessions form renders properly.
 #    Post to the sessions path with an invalid params hash.
@@ -8,7 +8,17 @@ require 'test_helper'
 #    Visit another page (such as the Home page).
 #    Verify that the flash message doesn’t appear on the new page. 
 
+# 2
+#   Visit the login path.
+#   Post valid information to the sessions path.
+#   Verify that the login link disappears.
+#   Verify that a logout link appears
+#   Verify that a profile link appears. 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+  
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -18,4 +28,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                         password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end 
 end
